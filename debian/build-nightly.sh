343c941af4838e87827b81749c59727aad15ed3d
fgan1 <fgan1@hotmail.com>
2014-08-12 16:26:32 -0300

    fogbow-cli

--- debian/build-nightly.sh
+++ debian/build-nightly.sh
@@ -6,10 +6,6 @@ set -e
 # checks if there is new commit for it github that was not built yet
 # and builds it. 
 
-# Build variables
-DEBFULLNAME="Abmar Barros (OurGrid Packaging)"
-DEBEMAIL="abmar@lsd.ufcg.edu.br"
-
 DIR_NAME="$(dirname $0)"
 CURRENT_DIR="$(cd $DIR_NAME; pwd)"
 PROJECTS_DIR=$CURRENT_DIR/projects
@@ -19,48 +15,49 @@ DATE=$(date +%Y%m%d)
 DATE_REPR=$(date -R)
 
 DOWNLOAD_ROOT=/tmp/build
-GIT_URL=https://github.com/OurGrid/opportunistic-nc.git
-GIT_PATH=$BUILD_DIR/git
-
-# Updating git folder
-if [ -d "$GIT_PATH" ]; then
-  cd $GIT_PATH
-  git pull
-  cd $CURRENT_DIR
-else
-  git clone $GIT_URL $GIT_PATH
-fi
-
-cd $GIT_PATH
-REV="$(git log -1 --pretty=format:%h)"
-
-if [ -f $BUILD_DIR/prev.rev ]; then
-  REV_PREV="$(cat $BUILD_DIR/prev.rev)"
-fi
-
-if [ "$REV" == "$REV_PREV" ]; then
-  echo "No need to build!"
-  exit 0
-fi
 
 # Save previous build info
-echo "$REV" > $BUILD_DIR/prev.rev
+# echo "$REV" > $BUILD_DIR/prev.rev
 
 for PROJECT_FOLDER in $PROJECTS_DIR/*; do
-  
+
   echo "Processing project $PROJECT_FOLDER"
   source $PROJECT_FOLDER/configure
-  
+
+  if [ "$IS_ACTIVE" == "false" ]; then
+    echo "Skipping $PROJECT_FOLDER. Project is not active."
+    break 
+  fi
+
   # Setting project properties
   PROJECT_PATH=$BUILD_DIR/$PROJECT_NAME
   mkdir -p $PROJECT_PATH
+
+  GIT_PATH=$PROJECT_PATH/git
+
+  # Updating git folder
+  if [ -d "$GIT_PATH" ]; then
+    cd $GIT_PATH
+    git pull
+    cd $CURRENT_DIR
+  else
+    git clone $GIT_URL $GIT_PATH
+  fi
+
+  cd $GIT_PATH
+  REV="$(git log -1 --pretty=format:%h)"
   
   BUILD_VERSION="${DIST_VERSION}+dev${DATE}.git.${REV}"
   DIST_REVISION="${BUILD_VERSION}-1"
   
-  SOURCE="${PACKAGE}-${BUILD_VERSION}"
+  SOURCE="${PACKAGE}-${BUILD_VERSION}"  
   ORIG_TGZ="${PACKAGE}_${BUILD_VERSION}.orig.tar.gz"
   
+  if [ -a "$PROJECT_PATH/$ORIG_TGZ" ]; then
+    echo "Skipping $PROJECT_FOLDER. Package is already in its latest version."
+    break
+  fi
+
   echo "Building orig.tar.gz ..."
   cd $GIT_PATH
   git archive --format=tar "--prefix=${SOURCE}/" "${REV}" | gzip >"${PROJECT_PATH}/${ORIG_TGZ}"
@@ -89,10 +86,4 @@ ${CHANGELOG}\n\n\
       debuild -a${ARCH} || true
     done
   fi 
-    
-  # Copy packages to download folder
-  DOWNLOAD_PACKAGE_DIR=$DOWNLOAD_ROOT/$PACKAGE/$SOURCE
-  mkdir -p $DOWNLOAD_PACKAGE_DIR
-  rsync -a $PROJECT_PATH/${PACKAGE}_${BUILD_VERSION}* $DOWNLOAD_PACKAGE_DIR --exclude=*.build
-
 done
