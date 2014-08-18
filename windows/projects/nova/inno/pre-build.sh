0a170f5109c7cd96880bd656eb9b3a0aa854ee4f
Abmar Barros <abmargb@gmail.com>
2014-08-18 18:12:34 -0300

    Update pre-build.sh

--- windows/projects/nova/inno/pre-build.sh
+++ windows/projects/nova/inno/pre-build.sh
@@ -1,12 +1,12 @@
-unzip PyBow27.zip
+unzip Pybow27.zip
 
 git clone https://github.com/fogbow/nova-qemu-win-driver.git
-rm -rf PyBow27/Lib/site-packages/nova/virt/qemuwin
-cp -r nova-qemu-win-driver/qemuwin PyBow27/Lib/site-packages/nova/virt
+rm -rf Pybow27/Lib/site-packages/nova/virt/qemuwin
+cp -r nova-qemu-win-driver/qemuwin Pybow27/Lib/site-packages/nova/virt
 
 git clone https://github.com/fogbow/fogbow-powernap-win32.git
 cd fogbow-powernap-win32.git 
-../PyBow27/python.exe setup.py install
+../Pybow27/python.exe setup.py install
 cd ..
 
-rm PyBow27.zip
+rm Pybow27.zip
