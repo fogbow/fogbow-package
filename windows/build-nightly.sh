412e4bf0d85aee383c3913643f59e428e783f20a
Abmar Barros <abmargb@gmail.com>
2014-08-18 16:38:13 -0300

    Update build-nightly.sh

--- windows/build-nightly.sh
+++ windows/build-nightly.sh
@@ -14,25 +14,15 @@ for PROJECT_FOLDER in $PROJECTS_DIR/*; do
   echo "Processing project $PROJECT_FOLDER"
   source $PROJECT_FOLDER/configure
   
+  if [ "$IS_ACTIVE" == "false" ]; then
+    echo "Skipping $PROJECT_FOLDER. Project is not active."
+    continue
+  fi
   
   # Setting project properties
   PROJECT_PATH=$BUILD_DIR/$PROJECT_NAME
   mkdir -p $PROJECT_PATH
-  
-  BUILD_VERSION="${DIST_VERSION}+dev${DATE}.git.${REV}"
-  DIST_REVISION="${BUILD_VERSION}-1"
-  
-  SOURCE="${PACKAGE}-${BUILD_VERSION}"
-  ORIG_TGZ="${PACKAGE}_${BUILD_VERSION}.orig.tar.gz"
-  
-  echo "Building orig.tar.gz ..."
-  cd $GIT_PATH
-  git archive --format=tar "--prefix=${SOURCE}/" "${REV}" | gzip >"${PROJECT_PATH}/${ORIG_TGZ}"
 
-  cd $PROJECT_PATH
-  tar xzf $ORIG_TGZ
-
-  cd $PROJECT_PATH/$SOURCE
   cp -r $PROJECT_FOLDER/inno .
   cp -r $CURRENT_DIR/common/* ./inno
   bash ./inno/pre-build.sh
